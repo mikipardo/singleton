a02b7dc2b58b5db92f2b7dc9e50cc83ad3411004
Mikipardo <pardolopezmiguel@gmail.com>
2022-08-09 01:51:57 +0200

    singleton

---
+++ BDController.java
@@ -0,0 +1,44 @@
+package SINGLETON;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BDController {
+	
+	private static Connection conexion = null;
+
+	private BDController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static Connection getInstance() {
+		
+		try {
+			if(conexion == null) {
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				try {
+					conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/fifa", "root", "");
+					System.out.println("va bene");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.out.println("Error en Driver Manager");
+				}
+				
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conexion;
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+package SINGLETON;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+	}
+	
+
+}
