53682d58a962083b287d7f52c1521e340a4fc547
Mikipardo <pardolopezmiguel@gmail.com>
2022-08-09 02:06:54 +0200

    metiendo prueba

---
+++ pruebaGit.java
@@ -0,0 +1,18 @@
+package SINGLETON;
+
+public class pruebaGit {
+
+	public pruebaGit() {
+		// TODO Auto-generated constructor stub
+		
+		/*
+		 * 
+		 * 
+		 * vamos a irnos ya a disfrutar la vida y ganar dinero
+		 * 
+		 * 
+		 * */
+	}
+
+	
+}
